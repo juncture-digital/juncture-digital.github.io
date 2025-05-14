aee409ad9134b733816682712ae93437c303f2ec
Ron Snyder <ron@snyderjr.com>
2025-05-14 09:40:18 -0400

    testing

--- js/juncture.js
+++ js/juncture.js
@@ -1,4 +1,5 @@
 window.ghbase = "{{site.github.owner_name}}/{{site.github.repository_name}}/{{site.github.source.branch}}/{{page.path}}";
+var baseUrl = (window.location.port === '4100') ? 'http://localhost:3000' : 'https://www.juncture-digital.io';
 document.write('<link rel="stylesheet" href="https://www.juncture-digital.io/css/index.css">');
-if (document.readyState === 'interactive') document.body.appendChild(Object.assign(document.createElement('script'), { src: 'https://www.juncture-digital.io/js/index.js', type: 'module' }));
-else document.addEventListener('readystatechange', () => { if (document.readyState === 'interactive') document.body.appendChild(Object.assign(document.createElement('script'), { src: 'https://www.juncture-digital.io/js/index.js', type: 'module' })); })
+if (document.readyState === 'interactive') document.body.appendChild(Object.assign(document.createElement('script'), { src: `${baseUrl}/js/index.js`, type: 'module' }));
+else document.addEventListener('readystatechange', () => { if (document.readyState === 'interactive') document.body.appendChild(Object.assign(document.createElement('script'), { src: `${baseUrl}/js/index.js`, type: 'module' })); })
