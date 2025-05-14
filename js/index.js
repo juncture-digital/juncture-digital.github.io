dfd1d28439b9f605a228ceb7e869e3dee9c368d9
Ron Snyder <ron@snyderjr.com>
2025-05-14 08:40:12 -0400

    testing

--- js/index.js
+++ js/index.js
@@ -741,11 +741,5 @@ document.addEventListener('paste', () => {
 // Prevent default browser behavior on dragover to allow drop
 document.addEventListener('dragover', (e) => e.preventDefault());
 
-if (document.readyState === 'interactive') processPage(document.querySelector('.post-content') || document.body)
-else {
-  console.log('Juncture', document.readyState);
-  document.addEventListener('readystatechange', () => {
-    console.log('Juncture', document.readyState);
-    if (document.readyState === 'interactive') processPage(document.querySelector('.post-content') || document.body)
-  });
-}
+if (document.readyState === 'loading') document.addEventListener('readystatechange', () => processPage(document.querySelector('.post-content') || document.body))
+else processPage(document.querySelector('.post-content') || document.body)
