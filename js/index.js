30f7adeb66499c028454f4989420f9b89b8569eb
Ron Snyder <ron@snyderjr.com>
2025-06-04 10:50:35 -0400

    testing

--- js/index.js
+++ js/index.js
@@ -792,6 +792,7 @@ let [owner, repo, branch, ...rest] = ghbase.split('/')
 document.querySelectorAll('.post-image').forEach((el) => {
   if (el.dataset?.src) {
     let postPath = el.parentElement.dataset?.path .split('/').slice(0,-1)
+    console.log(`postPath: ${postPath} data-src: ${el.dataset?.src}`)
     el.src = postPath.length 
       ? `${imageServiceUrl}/gh:${owner}/${repo}/${branch}/${postPath.join('/')}/${el.dataset?.src}`
       : `${imageServiceUrl}/gh:${owner}/${repo}/${branch}/${el.dataset?.src}`;
