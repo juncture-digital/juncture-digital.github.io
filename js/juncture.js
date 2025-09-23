284414dc9b678469d60c79e41bd999cf8b9be0cc
Ron Snyder <ron@snyderjr.com>
2025-09-23 08:28:15 -0400

    Use document.currentScript to find Juncture loader

--- js/juncture.js
+++ js/juncture.js
@@ -1,8 +1,8 @@
 var baseUrl = (window.location.port === '4100') ? 'http://localhost:3000' : ['4200'].includes(window.location.port) ? `http://localhost:${window.location.port}` : 'https://www.juncture-digital.io';
 document.write(`<link rel="stylesheet" href="${baseUrl}/css/juncture.css">`);
-const ghbase = document.currentScript.dataset.ghbase || '{{site.github.owner_name}}/{{site.github.repository_name}}/{{site.github.source.branch}}/{{page.path}}';
+const ghbase = document.currentScript.dataset.ghbase;
 const selector = document.currentScript.dataset.selector;
-console.log(`ghbase=${ghbase} selector=#${selector}`)
+console.log(`ghbase=${ghbase} selector=${selector}`)
 let added = false;
 const addScript = () => {
   if (!added) { 
